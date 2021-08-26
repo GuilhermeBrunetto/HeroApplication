8bf08582dd5567181e30db2dfb8fb582e70da2a8
dcpm <37427885+dcpm@users.noreply.github.com>
2021-08-26 16:37:26 -0300

    arquitetura de pastas e componentes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './auth/auth.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent,
+    AuthComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
